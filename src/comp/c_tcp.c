dd214f623b00eb16f6abc6cd4793a485ac345cc3
Didier Barvaux <didier@barvaux.org>
2017-05-28 13:00:20 +0200

    Fix mix of rnd_8_t and seq_8_t types

--- src/comp/c_tcp.c
+++ src/comp/c_tcp.c
@@ -3307,7 +3307,7 @@ static int c_tcp_build_seq_8(const struct rohc_comp_ctxt *const context,
 		ret = c_tcp_code_tcp_opts_list_item(context, tcp, tcp_context->msn,
 		                                    ROHC_TCP_CHAIN_CO, &tcp_context->tcp_opts,
 		                                    seq8->options,
-		                                    rohc_max_len - sizeof(rnd_8_t),
+		                                    rohc_max_len - sizeof(seq_8_t),
 		                                    &no_item_needed);
 		if(ret < 0)
 		{
